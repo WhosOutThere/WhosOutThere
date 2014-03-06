c68c3cc919ae5e116380c6ea94a3cb5680452b6b
Shuotian Chen <shuotian@outlook.com>
2014-03-06 14:26:28 -0600

    Joshua and Shuotian Iteration 3 Commit 1. Added comments for self written functions 2. Cleaned up Javascript by moving chunks of code from within the main Facebook app code to separate functions

--- trunk/js/facebook.js
+++ trunk/js/facebook.js
@@ -1,90 +1,117 @@
-		var globalNames = new Array();
-		var globalLocations = new Array();
+var globalNames = new Array();
+var globalLocations = new Array();
 
-		window.fbAsyncInit = function() {
-			FB.init({
-				appId      : '212944075564919', //shuotian's appID
-    status     : true, // check login status
-    cookie     : true, // enable cookies to allow the server to access the session
-    xfbml      : true  // parse XFBML
-});
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: '212944075564919', //shuotian's appID
+        status: true, // check login status
+        cookie: true, // enable cookies to allow the server to access the session
+        xfbml: true // parse XFBML
+    });
 
-  // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
-  // for any authentication related change, such as login, logout or session refresh. This means that
-  // whenever someone who was previously logged out tries to log in again, the correct case below 
-  // will be handled. 
-  FB.Event.subscribe('auth.authResponseChange', function(response) {
-    // Here we specify what we do with the response anytime this event occurs. 
-    if (response.status === 'connected') {
-      // The response object is returned with a status field that lets the app know the current
-      // login status of the person. In this case, we're handling the situation where they 
-      // have logged in to the app.
-          document.getElementById("facebookLogout").style.display="block";
-
-      FB.api(
-      	"/me/friends",
-      	{ fields: 'name, location' },
-      	function (response) {
-      		if (response && !response.error) {
-      			for (var i = 0; i < response.data.length; i++) {
-      				globalNames[i] = response.data[i].name;
-      				if (response.data[i].location) {
-      					globalLocations[i] = response.data[i].location.name;
-      				}
-      				else {
-      					globalLocations[i] = "Null";
-      				}
-      			}
-            sessionStorage.arr = globalLocations;
-      		}
-      		else {
-      			console.log("Failed to get friend IDs");
-      		}
-
-          findFriends();
-      	}
-      	);
-  } else if (response.status === 'not_authorized') {
-      // In this case, the person is logged into Facebook, but not into the app, so we call
-      // FB.login() to prompt them to do so. 
-      // In real-life usage, you wouldn't want to immediately prompt someone to login 
-      // like this, for two reasons:
-      // (1) JavaScript created popup windows are blocked by most browsers unless they 
-      // result from direct interaction from people using the app (such as a mouse click)
-      // (2) it is a bad experience to be continually prompted to login upon page load.
-      FB.login(function(){}, {scope: 'user_friends,friends_location'});
-  } else {
-      // In this case, the person is not logged into Facebook, so we call the login() 
-      // function to prompt them to do so. Note that at this stage there is no indication
-      // of whether they are logged into the app. If they aren't then they'll see the Login
-      // dialog right after they log in to Facebook. 
-      // The same caveats as above apply to the FB.login() call here.
-      FB.login(function(){}, {scope: 'user_friends,friends_location'});
-  }
-});
+    // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
+    // for any authentication related change, such as login, logout or session refresh. This means that
+    // whenever someone who was previously logged out tries to log in again, the correct case below 
+    // will be handled. 
+    FB.Event.subscribe('auth.authResponseChange', function(response) {
+        // Here we specify what we do with the response anytime this event occurs. 
+        if (response.status === 'connected') {
+            // The response object is returned with a status field that lets the app know the current
+            // login status of the person. In this case, we're handling the situation where they 
+            // have logged in to the app.
+            document.getElementById("facebookLogout").style.display = "block";
+            // Populate the globalNames and globalLocations arrays
+            findFriends();
+        } else if (response.status === 'not_authorized') {
+            // In this case, the person is logged into Facebook, but not into the app, so we call
+            // FB.login() to prompt them to do so. 
+            // In real-life usage, you wouldn't want to immediately prompt someone to login 
+            // like this, for two reasons:
+            // (1) JavaScript created popup windows are blocked by most browsers unless they 
+            // result from direct interaction from people using the app (such as a mouse click)
+            // (2) it is a bad experience to be continually prompted to login upon page load.
+            FB.login(function() {}, {
+                scope: 'user_friends,friends_location'
+            });
+        } else {
+            // In this case, the person is not logged into Facebook, so we call the login() 
+            // function to prompt them to do so. Note that at this stage there is no indication
+            // of whether they are logged into the app. If they aren't then they'll see the Login
+            // dialog right after they log in to Facebook. 
+            // The same caveats as above apply to the FB.login() call here.
+            FB.login(function() {}, {
+                scope: 'user_friends,friends_location'
+            });
+        }
+    });
 };
 
-  // Load the SDK asynchronously
-  (function(d){
-  	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-  	if (d.getElementById(id)) {return;}
-  	js = d.createElement('script'); js.id = id; js.async = true;
-  	js.src = "//connect.facebook.net/en_US/all.js";
-  	ref.parentNode.insertBefore(js, ref);
-  }(document));
+// Load the SDK asynchronously
+(function(d) {
+    var js, id = 'facebook-jssdk',
+        ref = d.getElementsByTagName('script')[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement('script');
+    js.id = id;
+    js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    ref.parentNode.insertBefore(js, ref);
+}(document));
 
-  function facebookLogout() {
+/** 
+ * Facebook Logout
+ * Wrapper around Facebook logout API call and refreshes the page automatically
+ */
+function facebookLogout() {
     FB.logout(function(response) {});
     document.location.reload();
-  }
+}
 
-  function findFriends() {
-  	console.log('Finding Friends');
+/**
+ * Find Friends
+ * Wrapper around Facebook API call to request for a list of friends locations, then store
+ * the response inside the globalNames and globalLocations arrays.
+ */
+function findFriends() {
+    FB.api(
+        "/me/friends", {
+            fields: 'name, location'
+        },
+        function(response) {
+            if (response && !response.error) {
+                for (var i = 0; i < response.data.length; i++) {
+                    globalNames[i] = response.data[i].name;
+                    if (response.data[i].location) {
+                        globalLocations[i] = response.data[i].location.name;
+                    } else {
+                        globalLocations[i] = "Null";
+                    }
+                }
+                sessionStorage.arr = globalLocations;
+            } else {
+                console.log("Failed to get friend IDs");
+            }
+
+            // Print the friends and locations out to the webpage
+            // Note: This will not be present for the final product
+            printFriends();
+        }
+    );
+}
+
+/** 
+ * Print Friends
+ * Formats and prints out the globalNames and globalLocations arrays onto an element in the page
+ */
+function printFriends() {
+    console.log('Printing Friends');
     //FB.login(function(){}, {scope: 'user_friends, user_location, friends_location'});
     var userLocation = "<div class=\"table\">";
 
     for (var i = 0; i < globalNames.length; i++) {
-    	userLocation = userLocation + "<div class=\"row\"><span class=\"cell\">" + globalNames[i] + "</span><span class=\"cell\">" + globalLocations[i] + "</span></div>";
+        userLocation = userLocation + "<div class=\"row\"><span class=\"cell\">" + globalNames[i] + "</span><span class=\"cell\">" + globalLocations[i] + "</span></div>";
     }
     userLocation = userLocation + "</div>"
     document.getElementById("Names").innerHTML = userLocation;
