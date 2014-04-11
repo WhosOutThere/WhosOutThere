a8e3fa1ef46b7d3e6be06864ad6cbcd71f9ec3b3
luciazyy <zhang.yanyan.lucia@gmail.com>
2014-04-11 06:02:29 -0500

    zhang250 & zhou86 Iteration 5:refactor
    Refactoring

--- trunk/js/directionTest.js
+++ trunk/js/directionTest.js
@@ -13,39 +13,12 @@ var ret = {
     "distance": 135,
     "duration": 2
 };
-distance = response['routes']['legs']['distance'];
-duration = response['routes']['legs']['duration'];
+
 test('test current location to chicago distance', function () {
-   /* var response = {
-        "routes": {
-            "legs": {
-                "distance": 135,
-                "duration": 2
-            }
-        }
-    };
-    var ret = {
-        "distance": 135,
-        "duration": 2
-    };*/
     distance = response['routes']['legs']['distance'];
-    duration = response['routes']['legs']['duration'];
     equal(ret['distance'], distance, "success");
 })
 test('test current location to chicago duration', function () {
-    /* var response = {
-     "routes": {
-     "legs": {
-     "distance": 135,
-     "duration": 2
-     }
-     }
-     };
-     var ret = {
-     "distance": 135,
-     "duration": 2
-     };*/
-    distance = response['routes']['legs']['distance'];
     duration = response['routes']['legs']['duration'];
     equal(ret['duration'], duration, "success");
 })
@@ -63,15 +36,11 @@ var ret2 = {
     "distance": 173,
     "duration": 3
 };
-
 test('test chicago to wisconsin distance', function () {
     distance = route2['routes']['legs']['distance'];
-    duration = route2['routes']['legs']['duration'];
     equal(ret2['distance'], distance, "success");
 })
-
 test('test chicago to wisconsin distance', function () {
-    distance = route2['routes']['legs']['distance'];
     duration = route2['routes']['legs']['duration'];
     equal(ret2['duration'], duration, "success");
 })
@@ -93,13 +62,45 @@ var ret3 = {
 
 test('test wisconsin to los angeles distance', function () {
     distance = route3['routes']['legs']['distance'];
-    duration = route3['routes']['legs']['duration'];
     equal(ret3['distance'], distance, "success");
 })
-
-test('test wisconsin to los angeles distance', function () {
-    distance = route3['routes']['legs']['distance'];
+test('test wisconsin to los angeles duration', function () {
     duration = route3['routes']['legs']['duration'];
     equal(ret3['duration'], duration, "success");
+})
+
+var route4 = {
+    "routes": {
+        "leg0": {
+            "distance": 173,
+            "duration": 3
+        },
+        "leg1":{
+            "distance": 2047,
+            "duration": 29
+        }
+
+    }
+};
+var ret4 = {
+    "distance": 173,
+    "duration": 4
+};
+var ret5 = {
+    "distance": 2047,
+    "duration": 30
+};
+
+test('test multiple routes distance', function () {
+    distance = route4['routes']['leg0']['distance'];
+    duration = route4['routes']['leg0']['duration'];
+    equal(ret4['distance'], distance, "success");
+    notEqual(ret4['duration'], duration, "success");
+})
+test('test multiple routes duration', function () {
+    distance = route4['routes']['leg1']['distance'];
+    duration = route4['routes']['leg1']['duration'];
+    equal(ret5['distance'], distance, "success");
+    notEqual(ret5['duration'], duration, "success");
 })
 
