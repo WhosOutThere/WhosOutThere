1cbeb816ec18e50c0349fd4645aec394c3c28497
zwjshuang <zwjshuang@gmail.com>
2014-04-11 04:41:00 -0500

    Weijian Zhou and Yanyan Zhang for iteration 5
    Code refactoring so that the file becomes neat and easily
    understandable.

--- trunk/js/sendEmail.js
+++ trunk/js/sendEmail.js
@@ -10,7 +10,7 @@ var m = new mandrill.Mandrill('bVkQiTTlVFArlVrFvK7tZQ');
 
 //the actual function to send email 
 function sendTheMail() {
-// Send the email!
+    // Send the email!
     //address stores the email string that user inputs
     var address = document.getElementById("email-address").value;
     //subject stores the email subject that user inputs
@@ -18,24 +18,32 @@ function sendTheMail() {
     //content stores the email message that user inputs
     var content = document.getElementById("email-content").value
     
+    var global_sendlist = parserHelper(address);
+    
+    
+    
+    var params = {
+        "message": {
+            "from_email":"userEmail@gmail.com",
+            "to":global_sendlist,
+            "subject": subject,
+            "text": content
+        }
+    };
+    m.messages.send(params);
+}
+//convert the user input address to json format 
+function parserHelper(address){
+    
+    
     var emailArray = address.split(";");
-    var global_sendlist = new Array();
-
-    //convert the user input address to json format
+    // create a variable for the API call parameters
+    var temp = new Array();
     for (var i=0; i<emailArray.length; i++) {
     	var sendobj = {};
     	sendobj['email'] = emailArray[i];
-    	global_sendlist.push(sendobj);
+    	temp.push(sendobj);
     }
-    // create a variable for the API call parameters
-
-    var params = {
-    "message": {
-        "from_email":"userEmail@gmail.com",
-        "to":global_sendlist,
-        "subject": subject,
-        "text": content
-    }
-};
-    m.messages.send(params);
+    return temp;
+    
 }
