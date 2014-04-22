e403c1ca10a916bef68a86664a5ee8625da95f9c
Joshua <joshua.hyk@gmail.com>
2014-04-22 04:10:07 -0500

    added share itinerary initial implementation, joshua and shuotian

--- trunk/js/meetingdispaly.js
+++ trunk/js/meetingdispaly.js
@@ -1,3 +1,5 @@
+var globalmeetings = {};
+
 function showItinerary() {
 	var fbid = friendList.FBid;
 	if(fbid <= 0){
@@ -9,6 +11,7 @@ function showItinerary() {
 		console.log(data);
 		var result = $.parseJSON(data);
 		console.log(result);
+		globalmeetings=result;
 		document.getElementById("Itinerary").innerHTML = "";
 		if (result.length==0){
 			document.getElementById("Itinerary").value = "No itineraies displayed";
@@ -17,7 +20,7 @@ function showItinerary() {
 		else{
 			for(key in result){
 				var tripname = result[key][0];
-				var unit=  "<div class=\"panel panel-default\"><div class=\"panel-heading\"><h4 class=\"panel-title\"><a data-toggle=\"collapse\" data-parent=\"#accordion\" id=\""+key+"\" href=\"#collapse"+key+"\">"+tripname+"</a></h4></div>"+"<div id=\"collapse"+key+"\" class=\"panel-collapse collapse\"><div class=\"panel-body\">";
+				var unit=  "<div class=\"panel panel-default\"><div class=\"panel-heading\"><h4 class=\"panel-title\"><a onclick=\"renderFbListInDropdown("+key+")\" data-toggle=\"collapse\" data-parent=\"#accordion\" id=\""+key+"\" href=\"#collapse"+key+"\">"+tripname+"</a></h4></div>"+"<div id=\"collapse"+key+"\" class=\"panel-collapse collapse\"><div class=\"panel-body\">";
 				var meeting=""
 				var element=""
 				var meetings = result[key][1];
@@ -26,19 +29,58 @@ function showItinerary() {
 
 					<!--get all friends in the list, if we have a jason object, the friendlist will be coressponded to the correct meeting-->
 					var friends= meetings[j]['friend_name'];
-
-					/*for (var k=0;k< meetings.length-1; k++){
-						friends+= roadTripfriendmeetChicago[k]+","     
-					}
-					friends+=roadTripfriendmeetChicago[roadTripfriendmeetChicago.length-1]*/
 					meeting+=friends+"</dd><dt>Date</dt><dd>"+meetings[j]['date']+"</dd><dt>Time</dt><dd>"+meetings[j]['time']+"</dd><dt>Location</dt><dd>"+meetings[j]['location']+"</dd></dl>"
 					meeting+="<button onclick=\"firstRoute('"+meetings[j]['location']+", "+meetings[j]['city']+"',event)\">Getting there</button><hr>"
 				}
+				//input field for selecting the friend you want to share itinerary with
+				meeting+="<input class=\"share-itinerary-friend\" id=\"share-itinerary-friend"+key+"\" placeholder=\"Enter friend's name\"></input>";
+				//button to share itinerary with
+				meeting+="<button onclick=\"shareItinerary("+key+")\">Share Itinerary</button>";
 				unit+= meeting+"</div></div></div>"
 				document.getElementById("Itinerary").innerHTML += unit;
-
 			}
 		}
 		return false;
 	});
+}
+
+//function to render the fb friend list in the dropdown for the meeting we select. This prevents us from rendering multiple dropdown lists if we have multiple meetings.
+function renderFbListInDropdown(itineraryid){
+	var fbfriends = friendList.friends;
+	var inputid = "#share-itinerary-friend"+itineraryid;
+	console.log(inputid);
+	console.log(fbfriends);
+	var friends = [];
+	for(var i = 0; i < fbfriends.length; i++){
+		var friend = {
+			'value':fbfriends[i].id,
+			'label':fbfriends[i].name
+		};
+		friends.push(friend);
+	}
+    $( "#share-itinerary-friend"+itineraryid ).autocomplete({
+      minLength: 0,
+      source: friends,
+      focus: function( event, ui ) {
+        $( "#share-itinerary-friend"+itineraryid ).val( ui.item.label );
+        return false;
+      },
+      select: function( event, ui ) {
+        $( "#share-itinerary-friend"+itineraryid ).val( ui.item.label );
+        console.log(ui.item.value );
+ 		for(key in globalmeetings){
+ 			if(key==parseInt(itineraryid)){
+ 				console.log("Share itinerary");
+ 				console.log(globalmeetings[key]);
+ 				//TO BE FINISHED. FORMAT MEETING AND SEND VIA FB MESSAGE
+ 			}
+ 		}
+        return false;
+      }
+    })
+    .data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+      return $( "<li>" )
+        .append( "<a>" + item.label +"</a>" )
+        .appendTo( ul );
+    };
 }
